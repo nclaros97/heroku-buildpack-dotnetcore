28d39af6901802e59449b1e41103a8bb05ba0c86
nilson bladimir claros mejia <nilsonvcm@gmail.com>
2022-05-12 18:49:11 -0500

    asda

--- bin/util/common.sh
+++ bin/util/common.sh
@@ -131,7 +131,7 @@ get_framework_version() {
 get_runtime_framework_version() {
 	local runtime_framework_version=$(grep -oPm1 "(?<=<RuntimeFrameworkVersion>)[^<]+" $1/AuthApp/*.csproj)
 	if [[ ${#runtime_framework_version} -eq 0 ]]; then
-		echo "6.0.5"
+		echo "6.0"
 	else
 		echo "${runtime_framework_version//[a-z]/}"
 	fi
