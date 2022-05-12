30535b6dd5ddb777777cc9980c82602170a290db
nilson bladimir claros mejia <nilsonvcm@gmail.com>
2022-05-12 18:40:58 -0500

    adsad

--- bin/util/common.sh
+++ bin/util/common.sh
@@ -130,7 +130,7 @@ get_framework_version() {
 # input - $1
 get_runtime_framework_version() {
 	local runtime_framework_version=$(grep -oPm1 "(?<=<RuntimeFrameworkVersion>)[^<]+" $1/*.csproj)
-	print runtime_framework_version
+	print $runtime_framework_version
 	print $1/*.csproj
 	if [[ ${#runtime_framework_version} -eq 0 ]]; then
 		echo "6.0.5"
